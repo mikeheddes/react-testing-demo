5965767c8e4015be501118d216ed2657fefc6b3c
mikeheddes <mikeheddes@gmail.com>
2018-11-05 20:32:45 +0100

    Updates

---
+++ precache-manifest.e8f997425c7043ac9327c7dd23b897d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b0c1253a77b6b464434",
+    "url": "/react-testing-demo/static/js/runtime~main.0b0c1253.js"
+  },
+  {
+    "revision": "960f81514d84de69027c",
+    "url": "/react-testing-demo/static/js/main.960f8151.chunk.js"
+  },
+  {
+    "revision": "4343f08787d33097bceb",
+    "url": "/react-testing-demo/static/js/1.4343f087.chunk.js"
+  },
+  {
+    "revision": "960f81514d84de69027c",
+    "url": "/react-testing-demo/static/css/main.990c15db.chunk.css"
+  },
+  {
+    "revision": "4c9a5f607f7ce0d9e7cc005c4f2a45bf",
+    "url": "/react-testing-demo/index.html"
+  }
+];
